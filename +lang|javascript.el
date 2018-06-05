50fc0c3fd4e5425c4366498283817803a736fd3d
Brandon Orther <brandon@uptrend.tech>
2018-06-05 02:28:26 -0700

    feat: Add javascript test runner and coverage

--- +lang|javascript.el
+++ +lang|javascript.el
@@ -1,5 +1,12 @@
 ;;; ~/.doom.d/+lang|javascript.el -*- lexical-binding: t; -*-
 
+;; ** Jest (mocha.el)
+(load! "local/jest")
+(after! mocha
+  (set! :popup "^\\*mocha tests*"
+    '((size . 80) (side . right))
+    '((select . nil) (quit . nil) (autokill . t))))
 
-;;       ;; coverlay:untested-line-background-color (doom-blend 'red 'bg 0.3)
-;;       ;; coverlay:tested-line-background-color (doom-blend 'green 'bg 0.0)
+;; ** coverlay - code coverage overlays
+(setq coverlay:untested-line-background-color (doom-blend 'red 'bg 0.3)
+      coverlay:tested-line-background-color (doom-blend 'green 'bg 0.0))
